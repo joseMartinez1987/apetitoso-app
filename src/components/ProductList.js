08c1e61e6fb5aadca927721442404c19c90bd891
Jose Martinez <jose.martinez.mayora@gmail.com>
2018-12-24 00:18:05 -0300

    actualizaion

---
+++ src/components/ProductList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const ProductList = () => {
+    return (
+        <div>
+           <ul className ='lista-receta'>
+               <li>Torta de chocolate</li>
+               <li>Torta 3 leches</li>
+               <li>Torta de manjar</li>
+           </ul>
+        </div>
+    );
+};
+
+export default ProductList;
