84b0436edc2c5efe39e5fadde30e5957bc7be595
Oscar Martinez <oscar.martinez@ceptinel.com>
2018-12-24 00:10:00 -0300

    - Agregar firebase

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FireBaseDB from './FireBaseDB'
 
 class App extends Component {
+
+  constructor(){
+    
+    super();
+    this.firebase = new FireBaseDB();
+    this.firebase.recipeModel.on('value', (d) => {
+
+      console.log(d.val());
+    })
+
+    this.firebase.recipeModel.on('child_added', (d) => {
+
+      console.log(d.key);
+    })
+  }
+
+  getRecipentName(e){
+    
+    this.recipentName = e.target.value;
+    
+  }
+
+  addRecipent(){
+
+
+    var recipe = {
+      name: this.recipentName
+    }
+
+    this.firebase.recipeModel.push().set(recipe);
+  }
+
   render() {
+    this.recipentName = "";
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Help me I want to learn react 
           </p>
           <a
             className="App-link"
@@ -19,6 +53,9 @@ class App extends Component {
           >
             Learn React
           </a>
+        <input type="text" onChange={(e) => this.getRecipentName(e)}></input>
+        <button type="button" onClick={() => this.addRecipent()}>Crear Receta</button>
+
         </header>
       </div>
     );
---
+++ src/FireBaseDB.js
@@ -0,0 +1,16 @@
+  import firebase from 'firebase'
+  import {DB_CONFIG} from './config/config'
+
+  export default class FireBaseDB{
+
+
+    constructor(){
+
+      this.app = firebase.initializeApp(DB_CONFIG);
+      this.recipeModel = this.app.database().ref().child('recipe');
+
+    }
+
+    
+
+  }
